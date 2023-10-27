91f5b997d13c80f65000b46d2549cabc53fb7a06
enessamancioglu <enessamancioglu35@gmail.com>
2023-10-27 12:27:15 +0300

    pair2

---
+++ AmicablePairs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static int SumOfDivisors(int num)
+    {
+
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    static void Main()
+    {
+        int pairCount = 0;
+        int num = 1;
+
+
+        while (pairCount < 2)
+        {
+            int sum1 = SumOfDivisors(num); //
+
+            if (sum1 > num)
+            {
+                int sum2 = SumOfDivisors(sum1);
+
+                if (sum2 == num && num != sum1)
+                {
+                    Console.WriteLine($"{pairCount + 1}: {num} - {sum1}");
+                    pairCount++;
+                }
+            }
+            num++;
+        }
+    }
+}
